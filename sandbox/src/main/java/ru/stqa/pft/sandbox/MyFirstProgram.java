e4dfb2d218c3e43c525289ead5d139caabbef9fd
PavelLazarchik <pavel_lazarchik@tut.by>
2017-11-23 17:58:55 +0300

    Quare implemented

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,13 +1,23 @@
 package ru.stqa.pft.sandbox;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class MyFirstProgram {
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    System.out.println(2 + 2);
-    System.out.println(2 * 2);
-    System.out.println(2 / 2);
-    System.out.println(2 -2);
-    System.out.println(1 / 2);
-    System.out.println(1.0 / 2);
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Rectangle  with parts " + r.a + " and" + r.b + " = " + area(r));
+
+    Square s = new Square(7);
+    System.out.println("Square with parts " + s.a + " = " + area(s));
   }
+
+  public static double area(Rectangle r) {
+    return r.a * r.b;
+  }
+
+  public static int area(Square s) {
+    return  s.a * s.a;
+  }
+
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.sandbox;
+
+public class Square {
+  public int a;
+
+  public  Square(int a) {
+    this.a = a;
+  }
+}
