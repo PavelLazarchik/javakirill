202d75d0370aae5c14443c9cda6cfe7907e9ad20
PavelLazarchik <pavel_lazarchik@tut.by>
2017-11-27 20:40:19 +0300

    Add new class GroupData

--- addressbook-web-tests/src/test/java/ru/sftqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/sftqa/pft/addressbook/GroupCreationTests.java
@@ -1,16 +1,10 @@
 package ru.sftqa.pft.addressbook;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class GroupCreationTests {
   FirefoxDriver wd;
@@ -20,30 +14,51 @@ public class GroupCreationTests {
    wd = new FirefoxDriver();
    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
    wd.get("http://localhost:8082/addressbook/group.php");
-   wd.findElement(By.name("user")).click();
-   wd.findElement(By.name("user")).clear();
-   wd.findElement(By.name("user")).sendKeys("admin");
-   wd.findElement(By.name("pass")).click();
-   wd.findElement(By.name("pass")).clear();
-   wd.findElement(By.name("pass")).sendKeys("secret");
-   wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+   login("admin", "secret");
  }
+
+ //create new method login as it use each time
+  private void login(String username, String password) {
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+  }
 
   @Test
   public void testGroupCreation() throws Exception {
 
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.name("new")).click();
+    gotoGroupPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("LPV Test 2", "LPV Test 2 Header", "LPV Test 2 Footer"));
+    submitGroupCreation();
+  }
+
+  private void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys("LPV Test 2");
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
     wd.findElement(By.name("group_header")).click();
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys("LPV Test 2 Header");
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     wd.findElement(By.name("group_footer")).click();
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys("LPV Test 2 Footer");
-    wd.findElement(By.name("submit")).click();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void initGroupCreation() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  private void gotoGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
   }
 
   @AfterMethod
