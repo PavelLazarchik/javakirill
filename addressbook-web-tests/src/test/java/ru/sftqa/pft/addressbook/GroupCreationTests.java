0e4543528e72bd2051832f84c481e2dfdf8f392c
PavelLazarchik <pavel_lazarchik@tut.by>
2017-11-27 20:10:26 +0300

    Перенес логин из теста в @BeforeTest
    Чтобы тест сделать понятным и простым

--- addressbook-web-tests/src/test/java/ru/sftqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/sftqa/pft/addressbook/GroupCreationTests.java
@@ -19,18 +19,19 @@ public class GroupCreationTests {
  public void setUp() throws Exception {
    wd = new FirefoxDriver();
    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+   wd.get("http://localhost:8082/addressbook/group.php");
+   wd.findElement(By.name("user")).click();
+   wd.findElement(By.name("user")).clear();
+   wd.findElement(By.name("user")).sendKeys("admin");
+   wd.findElement(By.name("pass")).click();
+   wd.findElement(By.name("pass")).clear();
+   wd.findElement(By.name("pass")).sendKeys("secret");
+   wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
  }
 
   @Test
-  public void testUntitledTestCase() throws Exception {
-    wd.get("http://localhost/addressbook/group.php");
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+  public void testGroupCreation() throws Exception {
+
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
