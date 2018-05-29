0b1158cccb860ba2129813478471689628c5f628
PavelLazarchik <pavel_lazarchik@tut.by>
2018-05-29 16:34:01 +0300

    Добавлена проверка по количеству групп перед созданием/удалением/изменением

--- addressbook-web-tests/src/test/java/ru/sftqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/sftqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package ru.sftqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.sftqa.pft.addressbook.model.GroupData;
 
@@ -9,13 +10,21 @@ public class GroupDeletionTests extends TestBase {
   public void testGroupDeletion() throws Exception {
 
     app.getNavigationHelper().gotoGroupPage();
+
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test 1", null, null));
     }
+    //считаем количество групп перед удалением группы
+    int before = app.getGroupHelper().getGroupCount();
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
+
+    //считаем колчество групп после удаления группы
+    int after = app.getGroupHelper().getGroupCount();
+    //сравниваем количество До и ПОСЛЕ. Должно быть отличие в 1 группу
+    Assert.assertEquals(after, before - 1);
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/sftqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/sftqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.sftqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.sftqa.pft.addressbook.model.GroupData;
 
@@ -12,11 +13,17 @@ public class GroupModificationTests extends TestBase {
       app.getGroupHelper().createGroup(new GroupData("test 1", null, null));
     }
     app.getNavigationHelper().gotoGroupPage();
+    //считаем количество групп перед изменением группы
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("LPV Test 3", "LPV Test 3 Header", "LPV Test 3 Footer"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
+    //считаем количество групп после изменения группы
+    int after = app.getGroupHelper().getGroupCount();
+    //сравниваем количество До и ПОСЛЕ. Не должно быть отличия в количестве
+    Assert.assertEquals(after, before);
   }
 
 }
